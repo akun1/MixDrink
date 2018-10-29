da8df841084e4f56238064104324ea03c0680b1b
EC2 Default User <ec2-user@ip-172-31-22-121.us-west-2.compute.internal>
2018-10-29 15:31:16 +0000

    Aggregate details for each drink and save to file

--- MixDrinkServer/Aggregator/aggregator.py
+++ MixDrinkServer/Aggregator/aggregator.py
@@ -2,6 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 import requests
+import json
+
+all_drinks = []
+save_file = 'catalog.json'
 
 @app.route('/')
 def hello_world():
@@ -9,10 +13,9 @@ def hello_world():
 
 @app.route('/stats')
 def stats():
-    return ''
+    return 'I have cataloged ' + str(len(all_drinks)) + ' drinks'
 
-if __name__ == "__main__":
-
+def get_all_cocktaildb():
     response = requests.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list')
     data = response.json()
     drink_categories = data['drinks']
@@ -30,6 +33,29 @@ if __name__ == "__main__":
         drink_list = data['drinks']
         all_drinks = all_drinks + drink_list
 
-    print('Cataloged ' + str(len(all_drinks)) + ' drinks')
+    drink_details = []
+
+    for drink in all_drinks:
+        response = requests.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='+drink['idDrink'])
+        data = response.json()
+        details = data['drinks'][0]
+        drink_details.append(details)
+
+    return drink_details
+
+if __name__ == "__main__":
+
+    update_catalog = False
+
+    if update_catalog:
+        detail_list = get_all_cocktaildb()
+        with open(save_file, 'w') as fout:
+            json.dump(detail_list, fout)
+
+    else:
+        with open(save_file, 'r') as fin:
+            detail_list = json.load(fin)
+
+    print('Cataloged ' + str(len(detail_list)) + ' drinks')
 
     app.run(host="0.0.0.0", port=80)
