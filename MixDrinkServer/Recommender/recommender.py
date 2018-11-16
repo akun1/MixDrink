5d31155782fd6edcbda575325492833208a23ba1
EC2 Default User <ec2-user@ip-172-31-30-177.us-west-2.compute.internal>
2018-11-16 18:47:28 +0000

    Drink confidences

--- MixDrinkServer/Recommender/recommender.py
+++ MixDrinkServer/Recommender/recommender.py
@@ -12,8 +12,12 @@ def hello_world():
 
 @app.route('/recommendation', methods=['GET', 'POST'])
 def recommendation():
-    input = request.json
-    print(input)
+
+    if request.json:
+        input = request.json
+        print(input)
+
+
     favorite_drinks = ['Cuba Libre', 'Gin and Tonic', 
         'Long Island Ice Tea', 'Espresso Martini', 'Lemon Drop',
         'Manhattan', 'Negroni', 'Mulled Wine', 'Mimosa', 'Tennessee Mud']
@@ -31,10 +35,24 @@ def recommendation():
         'Tennessee Mud' : 4.9
     }
 
+    drink_confidences = {
+        'Cuba Libre' : 1.0,
+        'Gin and Tonic' : 0.9,
+        'Long Island Ice Tea' : 0.8,
+        'Espresso Martini' : 0.7,
+        'Lemon Drop' : 0.6,
+        'Manhattan' : 0.5,
+        'Negroni' : 0.4,
+        'Mulled Wine' : 0.3,
+        'Mimosa' : 0.2,
+        'Tennessee Mud' : 0.1
+    }
+
     resp = []
     for drink in drink_details:
         if drink['strDrink'] in favorite_drinks:
             drink['rating'] = drink_scores[drink['strDrink']]
+            drink['confidence'] = drink_confidences[drink['strDrink']]
             resp.append(drink)
     return jsonify(resp)
 
