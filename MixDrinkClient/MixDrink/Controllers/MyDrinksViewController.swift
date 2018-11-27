134c359424a9df397e0fcd6da11e3ba081ef884b
Akash Kundu <akashkundu92@gmail.com>
2018-11-27 02:03:39 -0600

    ability to remove favorite drinks and update suggestions when done completed

--- MixDrinkClient/MixDrink/Controllers/MyDrinksViewController.swift
+++ MixDrinkClient/MixDrink/Controllers/MyDrinksViewController.swift
@@ -16,6 +16,9 @@ class MyDrinksViewController: UIViewController, UICollectionViewDelegate, UIColl
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(updateFavs(_:)), name: Notification.Name(rawValue: "FavsUpdated"), object: nil)
+        
         setupCollectionView()
 
         // Uncomment the following line to preserve selection between presentations
@@ -23,18 +26,12 @@ class MyDrinksViewController: UIViewController, UICollectionViewDelegate, UIColl
 
         // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    @objc func updateFavs(_ notification: Notification) {
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
     }
-    */
-
-    // MARK: UICollectionViewDataSource
 
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
--- MixDrinkClient/MixDrink/Views/MyDrinksCollectionViewCell.swift
+++ MixDrinkClient/MixDrink/Views/MyDrinksCollectionViewCell.swift
@@ -7,9 +7,30 @@
 //
 
 import UIKit
-
+import UserNotifications
 class MyDrinksCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var drinkImage: UIImageView!
     @IBOutlet weak var drinkName: UILabel!
     var drink : Drink?
+    
+    @IBAction func dislikeTapped(_ sender: Any) {
+        if let drink = drink {
+            let filteredDrinks : Drinks = Drinks()
+            filteredDrinks.drinks = Me.shared.myLikedDrinks.drinks.filter({$0.name != drink.name})
+            Me.shared.myLikedDrinks = filteredDrinks
+        }
+        let generator = UIImpactFeedbackGenerator(style: .heavy)
+        generator.impactOccurred()
+        
+        pushFavoriteDrinks()
+    }
+    
+    func pushFavoriteDrinks() {
+        print("Sending post now!")
+        API.sendFavoriteDrinks {
+            print("Sending of favs complete")
+            NotificationCenter.default.post(name: Notification.Name(rawValue: "RecsUpdated"), object: nil)
+            NotificationCenter.default.post(name: Notification.Name(rawValue: "FavsUpdated"), object: nil)
+        }
+    }
 }
